4d006a8b96271e69b3bf8dfa36136ef2e5bd8b17
kharchef <76191378+kharchef@users.noreply.github.com>
2021-04-08 22:56:12 +0100

    Add files via upload

---
+++ container.js
@@ -0,0 +1,23 @@
+const dependable = require('dependable');
+const path = require('path');
+
+const container = dependable.container();
+
+const simpleDependencies = [
+    ['_', 'lodash'],
+];
+
+simpleDependencies.forEach(function(val){
+    container.register(val[0], function(){
+        return require(val[1]);
+    })
+});
+
+container.load(path.join(__dirname, '/controllerss'));
+container.load(path.join(__dirname, '/helperss'));
+
+container.register('container', function(){
+    return container;
+});
+
+module.exports = container;

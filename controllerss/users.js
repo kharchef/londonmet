ce04ff2ef1bffd38bc244a9022185b50b20c5842
kharchef <76191378+kharchef@users.noreply.github.com>
2021-04-26 16:33:27 +0100

    Add files via upload

--- controllerss/users.js
+++ controllerss/users.js
@@ -1,21 +1,38 @@
 'use strict';
 
-module.exports = function(_){
+module.exports = function(_, passport){
 
     return {
         SetRouting: function(router){
             router.get('/', this.indexPage);
             router.get('/signup', this.getSignUp);
+            router.get('home', this.homePage);
+            
+            
+            
+            router.post('/signup', this.postSignUp);
         },
 
         indexPage: function(req, res){
             return res.render('index');
          },
-
+         
          getSignUp: function(req, res){
              return res.render('signup');
+         },
+
+         postSignUp: passport.authenticate('local.signup', {
+             successRedirect: '/home',
+             failureRediret: '/signup',
+             failureFlash: true
+         }),
+
+         homePage: function(req, res){
+             return res.render('home');
          }
- 
+
+        
+        
     }
 
 }

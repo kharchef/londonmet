f775e59fbbe03399e170b188875fa5445976a3ad
kharchef <76191378+kharchef@users.noreply.github.com>
2021-05-07 18:53:11 +0100

    Add files via upload

--- passport/passport-local.js
+++ passport/passport-local.js
@@ -1,48 +1,70 @@
 'use strict'
 
-const passport = require('passport');
-const User = require('../models/user');
-const LocalStrategy = require('passport-local').Strategy;
+    const passport = require('passport');
+    const User = require('../models/user');
+    const LocalStrategy = require('passport-local').Strategy;
 
-passport.serializeUser((user, done)=>{
-    done(null, user.id);
-});
-
-passport.deserializeUser((id, done) =>{
-    User.findById(id, (err, user) => {
-        done(err, user);
-    });
-});
-
-passport.use('local.signup', new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true
-}, (req, email, password, done) => {
-
-User.findOne({'email': email}, (err, user) =>{
-    if(err){
-        return done(err);
-    }
-
-    if(user){
-        return  done(null, false, req.flash('error', 'This user email alrready exists'));
-    }
-
-    const newUser = new User();
-    newUser.username = req.body.username;
-    newUser.email = req.body.email;
-    newUser.password = newUser.encryptPassword(req.body.password);
-
-    newUser.save((err) => {
-        done(null, newUser);
+    passport.serializeUser((user, done)=>{
+        done(null, user.id);
     });
 
+    passport.deserializeUser((id, done) =>{
+        User.findById(id, (err, user) => {
+            done(err, user);
+        });
+    });
 
-});
+    passport.use('local.signup', new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    }, (req, email, password, done) => {
+
+    User.findOne({'email': email}, (err, user) =>{
+        if(err){
+            return done(err);
+        }
+
+        if(user){
+            return done(null, false, req.flash('error', 'This user email alrready exists'));
+        }
+
+        const newUser = new User();
+        newUser.username = req.body.username;
+        newUser.email = req.body.email;
+        newUser.password = newUser.encryptPassword(req.body.password);
+
+        newUser.save((err) => {
+            done(null, newUser);
+        });
+    });
 
 
-}));
+    }));
+
+    passport.use('local.login', new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    }, (req, email, password, done) => {
+
+    User.findOne({'email': email}, (err, user) =>{
+        if(err){
+            return done(err);
+        }
+        const message = [];
+        if(!user || !user.validUserPassword(password)){
+            message.push('Email Does Not Exist or Password is Invalid');
+            return done(null, false, req.flash('error', message));
+        }
+
+        return done(null, user);
+
+        
+        });
+
+
+    }));
 
 
 
---
+++ secret/secretFile.js
@@ -0,0 +1,6 @@
+module.exports = {
+    google: {
+        clientID: '921986176437-uc5tetsg1q0b4jehv2ij19erbvqp6a0a.apps.googleusercontent.com',
+        clientSecret: 'd1pd4PyfvUbkDztDSHBBVseY',
+    }
+}
